a241a6f18b7c5405cf184a1cc7192b20ead29564
Rares Tant <79656115+tant-rares-30127@users.noreply.github.com>
2021-08-16 22:06:35 +0300

    Added many to many ?

--- Team2Application/Data/Migrations/20210816190247_Skill_LibraryResources.Designer.cs
+++ Team2Application/Data/Migrations/20210816190247_Skill_LibraryResources.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Team2Application.Data;
 
 namespace Team2Application.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210816190247_Skill_LibraryResources")]
+    partial class Skill_LibraryResources
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
