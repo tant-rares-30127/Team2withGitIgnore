a31bd02676c40c2a95d48e059df6251a8d250671
TeonaTanasa <teona.tanasa@principal33.com>
2021-08-27 17:17:22 +0300

    added Broadcast services and messagehub.

---
+++ Team2Application/Services/SkillBroadcastService.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Team2Application.Services
+{
+    public class SkillBroadcastService : ISkillBroadcastService
+    {
+        private readonly IHubContext<MessageHub> messageHub;
+
+        public SkillBroadcastService(IHubContext<MessageHub> messageHub)
+        {
+            this.messageHub = messageHub;
+        }
+        public void SkillAdded(int id, string name, string skillMatrixUrl, string description)
+        {
+            messageHub.Clients.All.SendAsync("SkillAdded", id, name, skillMatrixUrl, description);
+        }
+
+        public void SkillDeleted(int id)
+        {
+            messageHub.Clients.All.SendAsync("SkillDeleted", id);
+        }
+
+        public void SkillUpdated(int id, string name, string skillMatrixUrl, string description)
+        {
+            messageHub.Clients.All.SendAsync("SkillUpdated", id, name, skillMatrixUrl, description);
+        }
+    }
+}
